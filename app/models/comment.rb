32149be45448e6581dab1f8517974760b4c2793c
ometman <revomet@gmail.com>
2023-12-14 16:50:58 +0000

    Modify readme and fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,17 +1,17 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    after_create :increment_post_comments_counter
-    after_destroy :decrement_post_comments_counter
+  after_create :increment_post_comments_counter
+  after_destroy :decrement_post_comments_counter
 
-    private
+  private
 
-    def increment_post_comments_counter
-      post.increment(:comments_counter)
-    end
+  def increment_post_comments_counter
+    post.increment(:comments_counter)
+  end
 
-    def decrement_post_comments_counter
-      post.decrement(:comments_counter)
-    end
+  def decrement_post_comments_counter
+    post.decrement(:comments_counter)
+  end
 end
