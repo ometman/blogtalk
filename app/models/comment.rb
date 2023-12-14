7e633a05885458950e468a96dcc701df7eb44dc1
ometman <revomet@gmail.com>
2023-12-14 15:25:04 +0000

    Modify like and comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,16 +2,16 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post
 
-    after_create :increment_posts_comments_counter
-    after_destroy :decrement_posts_comments_counter
+    after_create :increment_post_comments_counter
+    after_destroy :decrement_post_comments_counter
 
     private
 
-    def increment_posts_comments_counter
-      post.increment(:posts_counter)
+    def increment_post_comments_counter
+      post.increment(:comments_counter)
     end
 
-    def decrement_posts_comments_counter
-      post.decrement(:posts_counter)
+    def decrement_post_comments_counter
+      post.decrement(:comments_counter)
     end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,16 +2,16 @@ class Like < ApplicationRecord
     belongs_to :post
     belongs_to :user
 
-    after_create :increment_posts_likes_counter
-    after_destroy :decrement_posts_likes_counter
+    after_create :increment_post_likes_counter
+    after_destroy :decrement_post_likes_counter
 
     private
 
-    def increment_posts_likes_counter
+    def increment_post_likes_counter
       post.increment(:likes_counter)
     end
 
-    def decrement_posts_likes_counter
+    def decrement_post_likes_counter
       post.decrement(:likes_counter)
     end
 end
