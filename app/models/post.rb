18fc3d05ee0b5cb8aa76199512c8a933d90c7a7f
ometman <revomet@gmail.com>
2023-12-14 14:19:01 +0000

    Add methods to post model for user post count and five recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,24 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: :author_id
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
-  end
+
+    after_create :increment_user_posts_counter
+    after_destroy :decrement_user_posts_counter
+
+    private
+
+    def increment_user_posts_counter
+      author_increment(:posts_counter)
+    end
+
+    def decrement_user_posts_counter
+      auhor_decrement(:posts_counter)
+    end
+
+    public
+
+    def recent_comments(limit = 5)
+      comments.order(created_at: :desc).limit(limit)
+    end
+end
   
