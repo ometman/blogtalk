6fb40489631e54b02c510c24f03d03f78cfdbb9c
ometman <revomet@gmail.com>
2023-12-15 06:58:57 +0000

    Add validations in User model for name and posts counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def self.recent_posts(user_name, limit = 3)
     user = find_by(name: user_name)
     return unless user
