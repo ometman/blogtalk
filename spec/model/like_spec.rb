a181fb9cd2f3311c52ed9d5b3c488c1133bf24a8
ometman <revomet@gmail.com>
2023-12-15 08:06:46 +0000

    Add like_spec.rb in spec/model

---
+++ spec/model/like_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
+
+  describe 'callbacks' do
+    let!(:post) { create(:post) }
+    let!(:like) { create(:like, post: post) }
+
+    it 'increments post likes_counter after creating a like' do
+      expect { create(:like, post: post) }.to change { post.reload.likes_counter }.by(1)
+    end
+
+    it 'decrements post likes_counter after destroying a like' do
+      expect { like.destroy }.to change { post.reload.likes_counter }.by(-1)
+    end
+  end
