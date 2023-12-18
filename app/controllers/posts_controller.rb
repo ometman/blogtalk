b7a452c4f52dde06cfc2c29478141d25f08bef33
ometman <revomet@gmail.com>
2023-12-18 14:45:34 +0000

    Add code for the users and posts in the users and posts controllers respectively.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,23 @@
 class PostsController < ApplicationController
+  before_action :set_user
+  before_action :set_post, only [:show]
+
   def index
+    @posts = Post.all
+    render json: @posts
   end
 
   def show
+    render json: @post
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = @user.posts.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    render json: @users
   end
 
   def show
+    @user = User.find(param[:id])
+    render json: @user
   end
 end
