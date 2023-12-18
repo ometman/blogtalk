c06cd8161301a2728a3a4c299a41b0af348259fb
ometman <revomet@gmail.com>
2023-12-18 12:59:22 +0000

    Create routes for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
