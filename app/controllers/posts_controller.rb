d43fd2388ad712d24f93d2eedacfbb0a69eb8b00
ometman <revomet@gmail.com>
2024-01-13 16:11:23 +0000

    Add methods to post controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_user
-  before_action :set_post, only: [:show]
+  before_action :set_user, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = @user.posts.paginate(page: params[:page], per_page: 2)
@@ -12,6 +12,19 @@ class PostsController < ApplicationController
     @comments_user = @comments.map(&:user)
   end
 
+  def new
+    @post = @user.posts.new
+  end
+
+  def create
+    @post = @user.posts.new(post_params)
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_user
@@ -20,5 +33,9 @@ class PostsController < ApplicationController
 
   def set_post
     @post = @user.posts.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
