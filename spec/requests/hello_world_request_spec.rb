329d258c19f2bee70fe026cd7d988e62f5567d1c
ometman <revomet@gmail.com>
2024-01-13 21:12:14 +0000

    fix linters

--- spec/requests/hello_world_request_spec.rb
+++ spec/requests/hello_world_request_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "HelloWorlds", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/hello_world/index"
+RSpec.describe 'HelloWorlds', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/hello_world/index'
       sleep 5
       expect(response).to have_http_status(:success)
     end
   end
-
 end
