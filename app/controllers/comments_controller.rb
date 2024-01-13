33bad5cb04aa53d1fed34673707ba2131ad2a11b
ometman <revomet@gmail.com>
2024-01-13 16:18:25 +0000

    Create form for creating new post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
     end
   
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:text)
     end
   end
   
