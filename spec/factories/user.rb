59a2632d6e67984f27cd7816cb856f467987f087
ometman <revomet@gmail.com>
2024-01-23 13:01:03 +0000

    Write test using Capybara for user index page feature

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "user_#{n}" }
+    photo { ActionController::Base.helpers.asset_path('photo.jpg') }
     posts_counter { 0 }
-
     trait :with_posts do
       after(:create) do |user|
         create_list(:post, 3, author: user)
