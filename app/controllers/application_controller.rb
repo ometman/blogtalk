e472e231d568189464d25c21d295fcd552fdf776
ometman <revomet@gmail.com>
2024-01-25 05:05:26 +0000

    Improve configuration of devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
-  # def current_user
-  #   @current_user ||= User.first
-  # end
-  # helper_method :current_user
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :password, :current_password)}
+  end
 end
