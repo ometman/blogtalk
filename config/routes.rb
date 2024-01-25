325a5052e0fe0481e31a9ce7bb722456bab6dc9a
ometman <revomet@gmail.com>
2024-01-25 14:16:58 +0000

    Add home controller, css and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "home#index"
 
   resources :users, only: [:index, :show, :new, :create] do
     resources :posts, only: [:index, :show, :new, :create] do
