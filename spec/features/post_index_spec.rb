02c2727cee27d94a83256f1e08b46803a2fcb3a4
ometman <revomet@gmail.com>
2024-01-25 15:15:06 +0000

    Fix post title visibility in post_index_spec.rb

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -25,7 +25,7 @@ RSpec.feature 'User Post Index Page', type: :feature do
     post = user.posts.first
     visit user_posts_path(user)
 
-    expect(page).to have_content(post.id)
+    expect(page).to have_content("#{post.title}")
   end
 
   scenario 'I can see some of the post\'s body' do
@@ -67,7 +67,7 @@ RSpec.feature 'User Post Index Page', type: :feature do
     post = user.posts.first
     visit user_posts_path(user)
 
-    click_link "Post: #{post.id}"
+    click_link "Post #{post.id}"
 
     expect(current_path).to eq(user_post_path(user, post))
   end
