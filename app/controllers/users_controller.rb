6e1899c790a3f9a9ab8946fff485e97daa5557e0
ometman <revomet@gmail.com>
2024-01-25 11:45:07 +0000

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.includes(:posts).all
   end
+
   def show
     @user = current_user
     @user_posts = @user ? @user.posts.includes(:comments).all : []
