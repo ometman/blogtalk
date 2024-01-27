1ae8f540d99078831aecf9acf2ee2afb3ce9df0c
Ahmed <ezmh015@gmail.com>
2024-01-27 15:03:46 +0300

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,8 +9,8 @@ class Ability
       can :manage, :all
     else
       can :read, :all
-      can :destroy, Post, user_id: user.id
-      can :destroy, Comment, user_id: user.id
+      can :destroy, Post, user_id: user.id if user.persisted?
+      can :destroy, Comment, user_id: user.id if user.persisted?
     end
   end
 end
