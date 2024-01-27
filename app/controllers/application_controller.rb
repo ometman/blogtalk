17e59d269f206a232c0503a418202ef5d33f1308
Ahmed <ezmh015@gmail.com>
2024-01-27 15:05:38 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include CanCan::ControllerAdditions
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :password, :current_password) }
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_user)
   end
 end
